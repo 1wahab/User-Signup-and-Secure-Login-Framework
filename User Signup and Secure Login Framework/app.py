a8544daec1dcc78efe90d0203ace1b2d6ae901bd
Wahab Kazim <119747474+1wahab@users.noreply.github.com>
2025-01-02 17:17:08 +0500

    Add files via upload

---
+++ User Signup and Secure Login Framework/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_cors import CORS
+from models import db  # Import the db instance from models
+from routes import init_routes
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user_sisu333.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)  # Use the db instance from models
+
+# Create database tables within an application context
+with app.app_context():
+    db.create_all()
+
+# Register routes
+init_routes(app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ User Signup and Secure Login Framework/routes.py
@@ -0,0 +1,34 @@
+from flask import request, jsonify, render_template
+from models import User, db
+
+def init_routes(app):
+    @app.route('/')
+    def home():
+        return render_template('index.html')
+
+    @app.route('/signup', methods=['GET','POST'])
+    def signup():
+        if request.method == 'GET':
+            return render_template('sign_up.html')
+        data = request.get_json()
+        if not all([data.get('username'), data.get('email'), data.get('password')]):
+            return jsonify({"error": "All fields are required"}), 400
+        if User.query.filter((User.username == data['username']) | (User.email == data['email'])).first():
+            return jsonify({"error": "Username or email already exists"}), 400
+
+        user = User(username=data['username'], email=data['email'], password=data['password'])
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"message": "User registered successfully"}), 201
+
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        if request.method == 'GET':
+            return render_template('sign_in.html')
+        data = request.get_json()
+        if not all([data.get('email'), data.get('password')]):
+            return jsonify({"error": "Email and password are required"}), 400
+        user = User.query.filter_by(email=data['email'], password=data['password']).first()
+        if not user:
+            return jsonify({"error": "Invalid email or password"}), 400
+        return jsonify({"message": "Login successful", "username": user.username}), 200
